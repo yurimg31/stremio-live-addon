5120785740a747cb0068ebfddcc4d4ecf2828e85
yurimg31 <yurimg31@gmail.com.com>
2025-10-17 18:49:45 -0300

    agora vai

--- channels.js
+++ channels.js
@@ -1,19 +1,26 @@
-const PROXY = 'https://stremio-proxy-0ws0.onrender.com/hls?url=';
+const PROXY = 'https://xxxxx.trycloudflare.com/hls?url=';
 
 module.exports = [
   
     
   {
+    id: 'live:band',
+    name: 'Band (ao vivo)',
+    logo: 'https://multicanaishd.pro/wp-content/uploads/2024/10/Band-500x500.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/bandsp/index.m3u8')
+  },
+
+   {
     id: 'live:bandsports',
     name: 'Band Sports (ao vivo)',
-    logo: 'https://i.imgur.com/d2V7d39.jpeg',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/bandsport/index.m3u8?token=1b1efe68dd748e13f3f6075b6e0c3d2b05e9d2b7-acacf188591e0d5003156aefa9a6baf3-1760713663-1760710063&remote=no_check_ip')
+    logo: 'https://embedcanais.canaistv.site/bandsports/index.m3u8',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/bandsp/index.m3u8')
   },
   
   {
     id: 'live:cazetv',
     name: 'Caze TV (ao vivo)',
-    logo: 'https://embedcanais.com/images/cazetv.png',
+    logo: 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/07/03/1163449859-copacazetv.jpg',
     url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/cazetv/index.m3u8')
   },
 
@@ -28,42 +35,42 @@ module.exports = [
     id: 'live:combate',
     name: 'Combate (ao vivo)',
     logo: 'https://i.imgur.com/yn84kMx.png',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/combate/index.m3u8?token=4c0b5b75fca2b6a26c024887b6dae00981108d9f-e5ed3a1fee5ae67a4a09f413138e4d61-1760713752-1760710152&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/combate/index.m3u8')
   },
 
   {
-    id: 'live:globo',
-    name: 'Globo (ao vivo)',
+    id: 'live:globosp',
+    name: 'Globo SP (ao vivo)',
     logo: 'https://i.imgur.com/lU31koL.jpeg',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/globosp/index.m3u8?token=5493457028f1077b8c50af9c0e90bac027514596-9ef61c3f719eeb6b40512290ebd989e1-1760713816-1760710216&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/globosp/index.m3u8')
   },
 
   {
     id: 'live:espn',
     name: 'ESPN (ao vivo)',
     logo: 'https://i.imgur.com/87qr4Et.jpeg',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/espn/index.m3u8?token=a05bf58750f9a3d3cb48175cfdaee0f1858f96de-989050189df0a3d46851e951c0d49ecd-1760718288-1760714688&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/espn/index.m3u8')
   },
 
   {
     id: 'live:espn2',
     name: 'ESPN2 (ao vivo)',
     logo: 'https://i.imgur.com/87qr4Et.jpeg',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/espn2/index.m3u8?token=2464018378220b0e1ba36d7e617e8fc286b5f366-bf9a525782027e2f3743110c25808f09-1760714546-1760710946&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/espn2/index.m3u8')
   },
 
   {
     id: 'live:espn3',
     name: 'ESPN3 (ao vivo)',
     logo: 'https://i.imgur.com/87qr4Et.jpeg',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/espn3/index.m3u8?token=43f75faeee55c97011c9c16cd7e907b5042556a5-55bfc7d582b02b3b753fd38a3541b799-1760714578-1760710978&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/espn3/index.m3u8')
   },
 
   {
     id: 'live:espn4',
     name: 'ESPN4 (ao vivo)',
     logo: 'https://i.imgur.com/87qr4Et.jpeg',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/espn4/index.m3u8?token=5f1e2ff7690730a6e7e7da840d1422cdf06e4236-0438d805baf7b12f6c86ce02ec1849d1-1760714597-1760710997&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/espn4/index.m3u8')
   },
 
   {
@@ -73,25 +80,33 @@ module.exports = [
     url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/nsports/index.m3u8')
   },
 
+   {
+    id: 'live:paramountplus',
+    name: 'Paramount Plus (ao vivo)',
+    logo: 'https://embedcanais.com/images/paramountplus.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/paramountplus/index.m3u8')
+  },
+
+
   {
     id: 'live:premiereclubes',
     name: 'Premiere Clubes (ao vivo)',
     logo: 'https://i.imgur.com/gti9kDk.png',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/pre1/index.m3u8?token=63a5b3ffae28f0d0181f2efb95f3d0ddc6f74774-f83f1893910fa7ddea2a8474d8593d5a-1760714648-1760711048&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/premiereclubes/index.m3u8')
   },
 
   {
     id: 'live:premiere2',
     name: 'Premiere 2 (ao vivo)',
     logo: 'https://i.imgur.com/vFbYdWV.png',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/pre2/index.m3u8?token=72829355dbb4444a77f9a1910b0ca28562684ab8-d49b5ebb4143914c85676070df5b44db-1760714690-1760711090&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/premiere2/index.m3u8')
   },
 
    {
     id: 'live:premiere3',
     name: 'Premiere 3 (ao vivo)',
     logo: 'https://i.imgur.com/zPXOnSj.png',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/pre3/index.m3u8?token=95d9b78c6da159c133767dd2251035ded5fb2230-0f7ef1e36ad54b0d01b901ed7d8cf9ab-1760714725-1760711125&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/premiere3/index.m3u8')
   },
 
    {
@@ -126,41 +141,83 @@ module.exports = [
     id: 'live:sbt',
     name: 'SBT (ao vivo)',
     logo: 'https://i.imgur.com/GDzaRQn.jpeg',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/sbt/index.m3u8?token=1f7743b56e4cc36935cf9aaec20854594a775914-a869f413ade566330f6845db393cfed5-1760717356-1760713756&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/sbtsp/index.m3u8')
   },
 
   {
     id: 'live:sportv',
     name: 'Sportv (ao vivo)',
     logo: 'https://i.imgur.com/NPNdZEE.jpeg',
-    url: PROXY + encodeURIComponent('https://01.user-a023453-premium-googleapis.art/sportv/index.m3u8?token=b86e51ea6e22edf60012176f3176f1b969e88b79-4f5c06b0d2863451dc2362a60a25bfcd-1760717404-1760713804&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/sportv/index.m3u8')
   },
 
   {
     id: 'live:sportv2',
     name: 'Sportv 2 (ao vivo)',
     logo: 'https://i.imgur.com/fU82CZZ.jpeg',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/sportv2/index.m3u8?token=55b1d72120b4359bde98dc02d928c1cd41b613c2-b1634687163294298b97802597fdce08-1760717439-1760713839&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/sportv2/index.m3u8')
   },
 
   {
     id: 'live:sportv3',
     name: 'Sportv 3 (ao vivo)',
     logo: 'https://i.imgur.com/ebVHzOI.jpeg',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/sportv3/index.m3u8?token=37892eab36e910eafedf56541403ca65dcef8cf5-be3c6cd1620bbb82e7723122634e85f5-1760717469-1760713869&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/sportv3/index.m3u8')
   },
 
   {
     id: 'live:ufcfightpass',
     name: 'UFC Fight Pass (ao vivo)',
     logo: 'https://i.imgur.com/jUZoTpA.png',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/ufcfightpass/index.m3u8?token=6d3c16847d0ac21f162419980221fbb71b1f542e-76867a6efb25480c4334576251f81fbf-1760717523-1760713923&remote=no_check_ip')
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/ufcfightpass/index.m3u8')
   },
 
   {
     id: 'live:tnt',
     name: 'TNT (ao vivo)',
     logo: 'https://i.imgur.com/Mbqa5ND.jpeg',
-    url: PROXY + encodeURIComponent('https://02.user-a023453-premium-googleapis.art/tnt/index.m3u8?token=6cd0a7c7bbddee4dcf0135a49e09223a4adbfd84-8bb203493cead2e4e91c17ef629802bf-1760717547-1760713947&remote=no_check_ip')
-  }
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/tnt/index.m3u8')
+  },
+
+  {
+    id: 'live:xsports',
+    name: 'X Sports (ao vivo)',
+    logo: 'https://embedcanais.com/images/xsports.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/xsports/index.m3u8')
+  },
+
+   {
+    id: 'live:max',
+    name: 'MAX (ao vivo)',
+    logo: 'https://embedcanais.com/images/max.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/max/index.m3u8')
+  },
+
+   {
+    id: 'live:dazn',
+    name: 'DAZN (ao vivo)',
+    logo: 'https://embedcanais.com/images/dazn.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/dazn/index.m3u8')
+  },
+
+   {
+    id: 'live:globorj',
+    name: 'Globo RJ (ao vivo)',
+    logo: 'https://i.imgur.com/lU31koL.jpeg',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/globorj/index.m3u8')
+  },
+
+   {
+    id: 'live:recordsp',
+    name: 'Record Sp (ao vivo)',
+    logo: 'https://embedcanais.com/images/record.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/recordsp/index.m3u8')
+  },
+
+   {
+    id: 'live:hbo',
+    name: 'HBO (ao vivo)',
+    logo: 'https://embedcanais.com/images/hbo.png',
+    url: PROXY + encodeURIComponent('https://embedcanais.canaistv.site/hbo/index.m3u8')
+  },
 ];
